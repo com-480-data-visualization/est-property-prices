02426e70f418aa0f604163cb5dea28816ec260e2
Jan Kokla <jan.kokla@gmail.com>
2025-03-31 17:16:25 +0200

    add treemap mvp

--- app/public/js/charts/treemapChart.js
+++ app/public/js/charts/treemapChart.js
@@ -1,30 +1,170 @@
 const dimensions = {
-    width: 600,
-    height: 600,
+  width: 600,
+  height: 600,
+};
+
+let chart;
+
+export function renderTreemapChart(data, metric = "Number of Transactions") {
+  d3.select("[treemap-chart]").selectAll("*").remove();
+
+  const svg = d3
+    .select("[treemap-chart]")
+    .append("svg")
+    .attr("viewBox", `0 0 ${dimensions.width} ${dimensions.height}`);
+  
+
+  const propertyMap = {
+    "Number of Transactions": "Number",
+    "Total area (ha)": "Total area (ha)",
+    "Total value (€)": "Total value (eur)",
   };
-  
-  let chart;
-  
-  export function renderTreemapChart(data) {
-    const svg = d3
-      .select("[treemap-chart]")
-      .append("svg")
-      .attr("viewBox", `0 0 ${dimensions.width} ${dimensions.height}`);
-  
-    const year = sessionStorage.getItem("year");
-  
-    chart = svg
-      .append("text")
-      .attr("x", dimensions.width / 2)
-      .attr("y", dimensions.height / 2)
-      .attr("text-anchor", "middle")
-      .attr("dominant-baseline", "middle")
-      .style("font-size", "24px")
-      .style("fill", "steelblue")
-      .text(year);
-  }
-  
-  export function updateYearTreemap(data, newYear) {
-    chart.transition().duration(1500).text(newYear);
-  }
-  
+
+  const propertyName = propertyMap[metric] || "Number";
+
+  // Give the data to this cluster layout:
+  var root = d3.hierarchy(data).sum(function (d) {
+    return d[propertyName];
+  });
+
+  // Then d3.treemap computes the position of each element of the hierarchy
+  var treemap = d3
+    .treemap()
+    .size([dimensions.width, dimensions.height])
+    .paddingTop(28)
+    .paddingRight(7)
+    .paddingInner(3)(
+    // Padding between each rectangle
+    //.paddingOuter(6)
+    //.padding(20)
+    root
+  );
+
+  // prepare a color scale
+  var color = d3
+    .scaleOrdinal()
+    .domain(["Buyers", "Sellers"])
+    .range(["#402D54", "#D18975"]);
+
+  // And an opacity scale
+  var opacity = d3.scaleLinear().domain([10, 30]).range([0.5, 1]);
+
+  // use this information to add rectangles:
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+      return d.x0;
+    })
+    .attr("y", function (d) {
+      return d.y0;
+    })
+    .attr("width", function (d) {
+      return d.x1 - d.x0;
+    })
+    .attr("height", function (d) {
+      return d.y1 - d.y0;
+    })
+    .style("stroke", "black")
+    .style("fill", function (d) {
+      return color(d.parent.data.Name);
+    })
+    .style("opacity", function (d) {
+      return opacity(d.data[propertyName]);
+    })
+    .attr("rx", 6)
+    .attr("ry", 6);
+
+  // and to add the text labels
+  // const tooSmall = width < 0.001 || height < 0.001
+
+  svg
+    .selectAll("text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+    .attr("x", function (d) {
+      return d.x0 + 5;
+    }) // +10 to adjust position (more right)
+    .attr("y", function (d) {
+      return d.y0 + 20;
+    }) // +20 to adjust position (lower)
+    .text(function (d) {
+      return d.data.Name;
+    })
+    // .attr('opacity', tooSmall ? 0 : 0.9)
+    .attr("font-size", "16px")
+    .attr("fill", "white");
+
+  // and to add the text labels
+  svg
+    .selectAll("vals")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+    .attr("x", function (d) {
+      return d.x0 + 5;
+    }) // +10 to adjust position (more right)
+    .attr("y", function (d) {
+      return d.y0 + 35;
+    }) // +20 to adjust position (lower)
+    .text(function (d) {
+      return d.data[propertyName];
+    })
+    .attr("font-size", "10px")
+    .attr("fill", "white");
+
+  // Add title for the 3 groups
+  svg
+    .selectAll("titles")
+    .data(
+      root.descendants().filter(function (d) {
+        return d.depth == 1;
+      })
+    )
+    .enter()
+    .append("text")
+    .attr("x", function (d) {
+      return d.x0;
+    })
+    .attr("y", function (d) {
+      return d.y0 + 21;
+    })
+    .text(function (d) {
+      return d.data.name;
+    })
+    .attr("font-size", "19px")
+    .attr("fill", function (d) {
+      return color(d.data.name);
+    });
+
+  // Add title for the 3 groups
+  svg
+    .selectAll("titles")
+    .data(
+      root.descendants().filter(function (d) {
+        return d.depth == 1;
+      })
+    )
+    .enter()
+    .append("text")
+    .attr("x", function (d) {
+      return d.x0;
+    })
+    .attr("y", function (d) {
+      return d.y0 + 21;
+    })
+    .text(function (d) {
+      return d.data.Name;
+    })
+    .attr("font-size", "19px")
+    .attr("fill", function (d) {
+      return color(d.data.Name);
+    });
+}
+
+export function updateYearTreemap(data, newYear) {
+  chart.transition().duration(1500).text(newYear);
+}
--- app/public/js/county.js
+++ app/public/js/county.js
@@ -1,19 +1,34 @@
 import { renderTimeline } from "./charts/timeline.js";
-import { renderMunicipalityMap, updateMunicipalityMap } from "./charts/municipalityMap.js";
+import {
+  renderMunicipalityMap,
+  updateMunicipalityMap,
+} from "./charts/municipalityMap.js";
 import { renderSpiderChart, updateYearSpider } from "./charts/spiderChart.js";
 import { renderBubbleChart, updateYearBubble } from "./charts/bubbleChart.js";
-import { renderTreemapChart, updateYearTreemap } from "./charts/treemapChart.js";
+import {
+  renderTreemapChart,
+  updateYearTreemap,
+} from "./charts/treemapChart.js";
 
 const municipalityFilePath = "/static/data/municipalities.json";
-const statisticsFilePath = "/static/data/transactions_with_residential_apartments_county_level.json"
-const municipalityStatisticsFilePath = "/static/data/transactions_with_residential_apartments_detailed.json"
-const landtypeFilePath = "/static/data/normalized_spider_data.json";
-
+const statisticsFilePath =
+  "/static/data/transactions_with_residential_apartments_county_level.json";
+const municipalityStatisticsFilePath =
+  "/static/data/transactions_with_residential_apartments_detailed.json";
+const landTypeFilePath = "/static/data/normalized_spider_data.json";
+const sellersFilePath =
+  "/static/data/transactions_by_residency_of_sellers_county_level.json";
+const buyersFilePath =
+  "/static/data/transactions_by_residency_of_buyers_county_level.json";
 
 const year = sessionStorage.getItem("year");
+const treemapDropdown = document.querySelector(".uk-select.treemap-select");
 
 export const dispatch = d3.dispatch("start", "end");
 dispatch.on("start", updateChartsWithYear);
+
+let sellersDataGlobal;
+let buyersDataGlobal;
 
 const getCountyRelatedMap = (data, id) => {
   return data.filter((d) => d.properties.MKOOD === id)[0];
@@ -27,7 +42,9 @@ const formatTimelineData = (statistics) => {
   return Object.entries(statistics.data)
     .map(([year, entry]) => ({
       date: new Date(`${year}-01-01`),
-      pricePerSquareMeter: entry.filter((d) => d["Area(m2)"] === "TOTAL")[0]["Price per unit area median(eur /m2)"],
+      pricePerSquareMeter: entry.filter((d) => d["Area(m2)"] === "TOTAL")[0][
+        "Price per unit area median(eur /m2)"
+      ],
     }))
     .sort((a, b) => d3.ascending(a.date, b.date));
 };
@@ -41,64 +58,142 @@ const getMunicipalitiesByCounty = (data, id) => {
   };
 };
 
-const formatSpiderData = (yearData) => 
-  yearData.map(entry => ({
+const formatSpiderData = (yearData) =>
+  yearData.map((entry) => ({
     category: entry.Name,
-    value: entry["Total area (ha)"]
+    value: entry["Total area (ha)"],
   }));
 
-  let landTypeData = {}; 
-  // let fetchedLandTypeData = {}; // Declare globally
-  
-  Promise.all([
-    fetch(municipalityFilePath).then((response) => response.json()),
-    fetch(statisticsFilePath).then((response) => response.json()),
-    fetch(landtypeFilePath).then((response) => response.json()),
-    fetch(municipalityStatisticsFilePath).then((response) => response.json())
-  ])
-    .then(([municipalityMapData, countyData, fetchedData, municipalityStats]) => {
+const formatTreemapData = (
+  countyId,
+  year,
+  sellersData,
+  buyersData,
+  metric = "Number"
+) => {
+  const sellersByCounty =
+    sellersData.find((d) => d.MKOOD === countyId)?.data || {};
+  const buyersByCounty =
+    buyersData.find((d) => d.MKOOD === countyId)?.data || {};
+
+  const sellersByYear = sellersByCounty[year] || [];
+  const buyersByYear = buyersByCounty[year] || [];
+
+  const filteredSellersData = sellersByYear.filter(
+    (entry) =>
+      entry.Name !== "Estonia" && !entry.Name.toLowerCase().includes("total")
+  );
+
+  const filteredBuyersData = buyersByYear.filter(
+    (entry) =>
+      entry.Name !== "Estonia" && !entry.Name.toLowerCase().includes("total")
+  );
+
+  filteredSellersData.sort((a, b) => b[metric] - a[metric]);
+  filteredBuyersData.sort((a, b) => b[metric] - a[metric]);
+
+  return {
+    children: [
+      { name: "Sellers", children: filteredSellersData },
+      { name: "Buyers", children: filteredBuyersData },
+    ],
+  };
+};
+
+let landTypeData = {};
+
+Promise.all([
+  fetch(municipalityFilePath).then((response) => response.json()),
+  fetch(statisticsFilePath).then((response) => response.json()),
+  fetch(landTypeFilePath).then((response) => response.json()),
+  fetch(municipalityStatisticsFilePath).then((response) => response.json()),
+  fetch(sellersFilePath).then((response) => response.json()),
+  fetch(buyersFilePath).then((response) => response.json()),
+])
+  .then(
+    ([
+      municipalityMapData,
+      countyData,
+      fetchedData,
+      municipalityStats,
+      sellersData,
+      buyersData,
+    ]) => {
       // fetchedLandTypeData = fetchedData; // Store globally
-  
+      sellersDataGlobal = sellersData;
+      buyersDataGlobal = buyersData;
+
       const id = sessionStorage.getItem("countyId");
       const selectedYear = sessionStorage.getItem("year");
-  
+
       countyData = getCountyRelatedStatistics(countyData, id);
       landTypeData = getCountyRelatedStatistics(fetchedData, id);
 
       const maxValue = Math.max(
         ...Object.values(landTypeData.data) // Iterate over all years' data
-          .flatMap(year => year.map(entry => entry["Total area (ha)"])) // Extract values
+          .flatMap((year) => year.map((entry) => entry["Total area (ha)"])) // Extract values
       );
-  
+
       const timelineData = formatTimelineData(countyData);
       renderTimeline(timelineData);
-  
+
       const yearData = landTypeData.data[selectedYear];
       const spiderData = formatSpiderData(yearData);
       renderSpiderChart(spiderData, maxValue);
-  
+
       renderBubbleChart(null);
-      renderTreemapChart(null);
       municipalityMapData = getMunicipalitiesByCounty(municipalityMapData, id);
       renderMunicipalityMap(municipalityMapData, municipalityStats);
-    })
-    .catch((error) => console.log(error));
-  
-  function updateChartsWithYear(selectedYear) {
-    sessionStorage.setItem("year", selectedYear);
-  
-    const maxValue = Math.max(
-      ...Object.values(landTypeData.data) // Iterate over all years' data
-        .flatMap(year => year.map(entry => entry["Total area (ha)"])) // Extract values
-    );
-    
-  
-    const yearData = landTypeData.data[selectedYear];
-    const spiderData = formatSpiderData(yearData);
-    renderSpiderChart(spiderData, maxValue);
-  
-    updateYearBubble(null, selectedYear);
-    updateYearTreemap(null, selectedYear);
-    updateMunicipalityMap();
-  }
-  
+
+      const treemapData = formatTreemapData(
+        id,
+        selectedYear,
+        sellersData,
+        buyersData
+      );
+      renderTreemapChart(treemapData);
+    }
+  )
+  .catch((error) => console.log(error));
+
+function updateChartsWithYear(selectedYear) {
+  sessionStorage.setItem("year", selectedYear);
+
+  const maxValue = Math.max(
+    ...Object.values(landTypeData.data) // Iterate over all years' data
+      .flatMap((year) => year.map((entry) => entry["Total area (ha)"])) // Extract values
+  );
+
+  const yearData = landTypeData.data[selectedYear];
+  const spiderData = formatSpiderData(yearData);
+  renderSpiderChart(spiderData, maxValue);
+
+  const id = sessionStorage.getItem("countyId");
+  const treemapData = formatTreemapData(
+    id,
+    selectedYear,
+    sellersDataGlobal,
+    buyersDataGlobal
+  );
+  console.log("rendering treemap");
+  renderTreemapChart(treemapData, treemapDropdown.value);
+
+  updateYearBubble(null, selectedYear);
+  updateYearTreemap(null, selectedYear);
+  updateMunicipalityMap();
+}
+
+treemapDropdown.addEventListener("change", (event) => {
+  const id = sessionStorage.getItem("countyId");
+  const selectedYear = sessionStorage.getItem("year");
+  const selectedValue = event.target.value;
+
+  const treemapData = formatTreemapData(
+    id,
+    selectedYear,
+    sellersDataGlobal,
+    buyersDataGlobal
+  );
+
+  renderTreemapChart(treemapData, selectedValue);
+});
